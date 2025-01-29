241d2f0df88a9a6af112ebc20ff8d3a5d036f709
Markab-1 <velichko.anna.b@gmail.com>
2025-01-29 14:05:02 +0100

    transitions + figures

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ function Footer({ children }) {
     return (
         <div className={s.footerContainer}>
             <p>Our Team. All rights reserved | Design: Our Team </p> 
+            <p><a className={s.link} href="https://www.flaticon.com/free-icons/hubble-space-telescope" title="HST icons">HST icons created by Smashicons - Flaticon</a></p>
         </div>
     );
 }
